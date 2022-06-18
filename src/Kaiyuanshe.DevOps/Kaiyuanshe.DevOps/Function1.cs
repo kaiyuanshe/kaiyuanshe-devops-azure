8803828501eac5c7f4f1d3cb94293978d55cf630
Junbo Wang <juniwang@microsoft.com>
2022-06-18 20:13:49 +0800

    add first sample Function

---
+++ src/Kaiyuanshe.DevOps/Kaiyuanshe.DevOps/Function1.cs
@@ -0,0 +1,26 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace Kaiyuanshe.DevOps
+{
+    public static class Function1
+    {
+        [Function("HttpFunction")]
+        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req,
+    FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("HttpFunction");
+            logger.LogInformation("message logged");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Date", "Mon, 18 Jul 2016 16:06:00 GMT");
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Kaiyuanshe!!");
+
+            return response;
+        }
+    }
+}
