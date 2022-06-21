e315890493cd6178155bf74e76b881db15ba0bab
Junbo Wang <juniwang@microsoft.com>
2022-06-21 10:58:22 +0800

    add timer functions to cleanup acr(not tested)

--- src/kaiyuanshe-devops/Kaiyuanshe.DevOps/AcrCleaner.cs
+++ src/kaiyuanshe-devops/Kaiyuanshe.DevOps/AcrCleaner.cs
@@ -1,16 +1,60 @@
-﻿using Microsoft.Azure.Functions.Worker;
+﻿using Azure.Containers.ContainerRegistry;
+using Azure.Identity;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Kaiyuanshe.DevOps
 {
     public class AcrCleaner
     {
+        // 0 30 9 * * *: at 9:30 AM every day
+        // 0 0 */2 * * *: once every two hours
         [Function("AcrCleanupTimer")]
-        public static void Run([TimerTrigger("0 0 */2 * * *")] TimerInfo timerInfo,
+        public static async Task Run([TimerTrigger("0 0 */2 * * *")] TimerInfo timerInfo,
     FunctionContext context)
         {
             var logger = context.GetLogger("AcrCleanupTimer");
-            logger.LogInformation($"Function Ran. Next timer schedule = {timerInfo.ScheduleStatus.Next}");
+            logger.LogInformation($"AcrCleanupTimer starts.");
+
+            var acrEndpoint = Config.AcrEndpoint;
+            if (string.IsNullOrEmpty(acrEndpoint))
+            {
+                logger.LogWarning("ACR_ENDPOINT is not configured. Please add it to Azure Function's Appliction Settings." +
+                    " Or disable this Function. Sample value: https://myacr.azurecr.cn");
+                return;
+            }
+
+            var credential = new DefaultAzureCredential();
+            var acr = new ContainerRegistryClient(new Uri(acrEndpoint), credential, new ContainerRegistryClientOptions
+            {
+                Audience = ContainerRegistryAudience.AzureResourceManagerChina
+            });
+
+            logger.LogInformation($"List repository names on {acrEndpoint}");
+            foreach (var name in acr.GetRepositoryNames())
+            {
+                logger.LogInformation($"List manifests of repository: {name}");
+                var repo = acr.GetRepository(name);
+                var manifests = repo.GetAllManifestProperties()
+                    .Where(m => m.CanDelete.GetValueOrDefault(true))
+                    .OrderByDescending(m => m.CreatedOn)
+                    .Skip(Config.AcrMinManifests)
+                    .Where(m => m.CreatedOn + Config.AcrManifestExpiry < DateTimeOffset.UtcNow);
+                foreach (var manifest in manifests)
+                {
+                    var image = acr.GetArtifact(name, manifest.Digest);
+                    foreach (var tag in manifest.Tags)
+                    {
+                        logger.LogInformation($"Deleting tag {tag} from repository {name}");
+                        await image.DeleteTagAsync(tag);
+                    }
+                    logger.LogInformation($"Deleting manifest {manifest.Digest} from repository {name}");
+                    await image.DeleteAsync();
+                }
+            }
         }
     }
 }
---
+++ src/kaiyuanshe-devops/Kaiyuanshe.DevOps/Config.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Kaiyuanshe.DevOps
+{
+    internal class Config
+    {
+        public static string AcrEndpoint
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("ACR_ENDPOINT");
+            }
+        }
+
+        public static int AcrMinManifests
+        {
+            get
+            {
+                if (int.TryParse(Environment.GetEnvironmentVariable("ACR_MIN_MANIFESTS"), out int min) && min > 0)
+                {
+                    return min;
+                }
+
+                return 10;
+            }
+        }
+
+        public static TimeSpan AcrManifestExpiry
+        {
+            get
+            {
+                if (int.TryParse(Environment.GetEnvironmentVariable("ACR_MANIFEST_EXPIRY_DAYS"), out int days) && days > 0)
+                {
+                    return TimeSpan.FromDays(days);
+                }
+
+                return TimeSpan.FromDays(90);
+            }
+        }
+    }
+}
