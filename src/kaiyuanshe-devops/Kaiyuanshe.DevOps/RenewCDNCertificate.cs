33e31d5771a3bd4f5c5b9bd6d38affe5f4c079ca
Junbo Wang <juniwang@microsoft.com>
2022-08-09 21:53:39 +0800

    download cert from kv

--- src/kaiyuanshe-devops/Kaiyuanshe.DevOps/RenewCDNCertificate.cs
+++ src/kaiyuanshe-devops/Kaiyuanshe.DevOps/RenewCDNCertificate.cs
@@ -1,6 +1,10 @@
-﻿using Kaiyuanshe.DevOps.Utils;
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Kaiyuanshe.DevOps.Utils;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace Kaiyuanshe.DevOps
@@ -15,17 +19,39 @@ namespace Kaiyuanshe.DevOps
             var logger = context.GetLogger(nameof(RenewCDNCertificate));
             logger.LogInformation($"RenewCDNCertificateTimer starts.");
 
+            var secretClient = new SecretClient(Config.CdnKeyVaultBaseUri, new DefaultAzureCredential());
+
             foreach (var domain in Config.CdnDomains)
             {
-                await RenewDomain(domain, logger);
+                try
+                {
+                    await RenewDomain(domain, secretClient, logger);
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, $"Failed to renew domain {domain}");
+                }
             }
         }
 
-        private static async Task RenewDomain(string domain, ILogger logger)
+        private static async Task RenewDomain(string domain, SecretClient secretClient, ILogger logger)
         {
             logger.LogInformation($"Check domain: {domain}");
+
             var inUseCert = await CertificateHelper.GetServerCertificateAsync($"https://{domain}");
-            logger.LogInformation($"in-use certificate for domain: {inUseCert.Thumbprint}");
+            logger.LogInformation($"[{domain}]in-use certificate: {inUseCert.Thumbprint}");
+
+            var kvCert = await DownloadCertFromKeyVault(domain, secretClient, logger);
+            bool needUpload = inUseCert.Thumbprint.ToUpper() != kvCert.Thumbprint.ToUpper();
+            logger.LogInformation($"[{domain}]latest certificate in key vault: {kvCert.Thumbprint}. Need upload: {needUpload}");
+        }
+
+        private static async Task<X509Certificate2> DownloadCertFromKeyVault(string domain, SecretClient secretClient, ILogger logger)
+        {
+            logger.LogInformation($"get latest certificate for `{domain}` from keyvault {secretClient.VaultUri}");
+            string secretName = domain.Replace(".", "-");
+            var secret = await secretClient.GetSecretAsync(secretName);
+            return new X509Certificate2(Convert.FromBase64String(secret.Value.Value));
         }
     }
 }
