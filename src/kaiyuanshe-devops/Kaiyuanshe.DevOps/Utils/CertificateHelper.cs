f1c4c888c31002c865985bc90906768ad68a55a2
Junbo Wang <juniwang@microsoft.com>
2022-08-13 22:04:12 +0800

    add method to export cert/key (#19)

--- src/kaiyuanshe-devops/Kaiyuanshe.DevOps/Utils/CertificateHelper.cs
+++ src/kaiyuanshe-devops/Kaiyuanshe.DevOps/Utils/CertificateHelper.cs
@@ -1,10 +1,11 @@
 ﻿using System.Net.Http;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace Kaiyuanshe.DevOps.Utils
 {
-    internal class CertificateHelper
+    internal static class CertificateHelper
     {
         internal static async Task<X509Certificate2> GetServerCertificateAsync(string url)
         {
@@ -13,7 +14,7 @@ namespace Kaiyuanshe.DevOps.Utils
             {
                 ServerCertificateCustomValidationCallback = (_, cert, __, ___) =>
                 {
-                    certificate = new X509Certificate2(cert.GetRawCertData());
+                    certificate = new X509Certificate2(cert.GetRawCertData(), default(string), X509KeyStorageFlags.Exportable);
                     return true;
                 }
             };
@@ -22,6 +23,31 @@ namespace Kaiyuanshe.DevOps.Utils
             await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
 
             return certificate;
+        }
+
+        internal static bool Export(this X509Certificate2 certificate, out string publicCert, out string privateKey)
+        {
+            publicCert = null;
+            privateKey = null;
+
+            byte[] certificateBytes = certificate.RawData;
+            X509Chain chain = new X509Chain();
+            bool build = chain.Build(certificate);
+            if (!build || chain.ChainElements.Count <= 1)
+            {
+                return false;
+            }
+            X509Certificate2 issuer = chain.ChainElements[1].Certificate;
+            char[] leafPem = PemEncoding.Write("CERTIFICATE", certificateBytes);
+            char[] issuerPem = PemEncoding.Write("CERTIFICATE", issuer.RawData);
+            publicCert = new string(leafPem) + "\n" + new string(issuerPem);
+
+            RSA key = certificate.GetRSAPrivateKey();
+            byte[] privKeyBytes = key.ExportPkcs8PrivateKey();
+            char[] privKeyPem = PemEncoding.Write("PRIVATE KEY", privKeyBytes);
+            privateKey = new string(privKeyPem);
+
+            return true;
         }
     }
 }
