c682f2e71b3b218c09ac83ef6bcdcadd0a6595fa
Junbo Wang <juniwang@microsoft.com>
2022-08-19 17:07:36 +0800

    add timer to cleanup expired cert

---
+++ src/kaiyuanshe-devops/Kaiyuanshe.DevOps/CleanupKeyVaultExpiredCertificates.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Certificates;
+using Kaiyuanshe.DevOps.Extensions;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Kaiyuanshe.DevOps
+{
+    internal class CleanupKeyVaultExpiredCertificates
+    {
+        // 0 0 8 * * *: at 8:00 AM every day
+        // 0 0 */4 * * *: once every 4 hours
+        [Function("CleanupExpiredCertificates")]
+        public static async Task Run([TimerTrigger("0 0 8 * * *")] TimerInfo timerInfo, FunctionContext context)
+        {
+            var logger = context.GetLogger(nameof(RenewCDNCertificate));
+            logger.LogInformation($"CleanupExpiredCertificates starts.");
+
+            var certClient = new CertificateClient(Config.CdnKeyVaultBaseUri, new DefaultAzureCredential());
+            var certs = certClient.GetPropertiesOfCertificatesAsync();
+            await certs.ForEach(async (prop) =>
+            {
+                if (!prop.ExpiresOn.HasValue || prop.ExpiresOn > DateTimeOffset.UtcNow)
+                {
+                    logger.LogInformation($"Certificate {prop.Name} is not expired, skipped.");
+                    return;
+                }
+
+                if (Guid.TryParse(prop.Name, out Guid result))
+                {
+                    logger.LogInformation($"Deleting certificate: {prop.Name}");
+                    await certClient.StartDeleteCertificateAsync(prop.Name);
+                }
+                else
+                {
+                    logger.LogInformation($"Certificate {prop.Name} is not a valid guid, skipped.");
+                    return;
+                }
+            });
+        }
+    }
+}
---
+++ src/kaiyuanshe-devops/Kaiyuanshe.DevOps/Extensions/AsyncEnumeratorExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kaiyuanshe.DevOps.Extensions
+{
+    internal static class AsyncEnumeratorExtension
+    {
+        public static async Task ForEach<T>(this IAsyncEnumerable<T> source, Action<T> action, int? limit = null, CancellationToken cancellationToken = default)
+        {
+            IAsyncEnumerator<T> enumerator = source.GetAsyncEnumerator();
+            int count = 0;
+            try
+            {
+                while (await enumerator.MoveNextAsync() && !cancellationToken.IsCancellationRequested)
+                {
+                    action(enumerator.Current);
+                    if (limit.HasValue)
+                    {
+                        if (Interlocked.Increment(ref count) >= limit.Value)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                await enumerator.DisposeAsync();
+            }
+        }
+
+        public static async Task ForEach<T>(this IAsyncEnumerable<T> source, Func<T, Task> func, int? limit = null, CancellationToken cancellationToken = default)
+        {
+            IAsyncEnumerator<T> enumerator = source.GetAsyncEnumerator();
+            int count = 0;
+            try
+            {
+                while (await enumerator.MoveNextAsync() && !cancellationToken.IsCancellationRequested)
+                {
+                    await func(enumerator.Current);
+                    if (limit.HasValue)
+                    {
+                        if (Interlocked.Increment(ref count) >= limit.Value)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                await enumerator.DisposeAsync();
+            }
+        }
+    }
+}
